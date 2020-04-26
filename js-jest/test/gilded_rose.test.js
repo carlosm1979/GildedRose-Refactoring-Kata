7f6e829c46e3c723fe62ea63ac2e47259a9bf985
carlos gonzalez <carlosm1979@gmail.com>
2020-04-26 18:35:10 +0200

    green - Quality degress one point after one day

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -6,4 +6,22 @@ describe("Gilded Rose", function() {
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe("foo");
   });
+
+  it('Sellin degress one point after one day', () => {
+    const gildedRose = new Shop([new Item("foo", 10, 0)]);
+
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].sellIn).toBe(9)
+  });
+
+  it('Quality degress one point after one day', () => {
+    const gildedRose = new Shop([new Item("foo", 1, 9)]);
+
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].quality).toBe(8)
+  });
+
+
 });
