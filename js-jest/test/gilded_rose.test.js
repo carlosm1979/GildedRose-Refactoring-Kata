7a9f00d2d2ee821b7dca377dc318c80ef33d3e76
carlos gonzalez <carlosm1979@gmail.com>
2020-04-26 18:43:41 +0200

    refactor test, add test cases - Once the sell by date has passed, Quality degrades twice as fast

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -15,4 +15,22 @@ describe("Gilded Rose", function() {
     expect(items[0].sellIn).toBe(9)
     expect(items[0].quality).toBe(8)
   });
+
+  describe.each([
+    {initialSellin: 0, initialQuality: 10, expectedSelling: -1, expectedQuality: 8},
+    {initialSellin: -1, initialQuality: 10, expectedSelling: -2, expectedQuality: 8}
+  ]
+    )(
+    'Once the sell by date has passed, Quality degrades twice as fast',
+    (data) => {
+      test(`when initial sellin is ${data.initialSellin}`, () => {
+        const gildedRose = new Shop([new Item("foo", data.initialSellin, data.initialQuality)]);
+
+        const items = gildedRose.updateQuality();
+
+        expect(items[0].sellIn).toBe(data.expectedSelling)
+        expect(items[0].quality).toBe(data.expectedQuality)
+      });
+    }
+  );
 });
