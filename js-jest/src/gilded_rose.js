021633d70a313f0cd34ddf4f7299ae184404ab21
carlos gonzalez <carlosm1979@gmail.com>
2020-04-26 19:08:29 +0200

    refactor -extract method

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -24,12 +24,12 @@ class Shop {
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+                this.increaseQuality(i);
               }
             }
             if (this.items[i].sellIn < 6) {
               if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+                this.increaseQuality(i);
               }
             }
           }
@@ -51,13 +51,17 @@ class Shop {
           }
         } else {
           if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
+            this.increaseQuality(i);
           }
         }
       }
     }
 
     return this.items;
+  }
+
+  increaseQuality(i) {
+    this.items[i].quality = this.items[i].quality + 1;
   }
 
   decreaseQuality(i) {
